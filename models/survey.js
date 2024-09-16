11936677a4177773d94769b920c7acec843cb6e8
suryapandiyan <surya.surya256@gmail.com>
2024-09-16 10:46:25 +0530

    api endpoints

--- models/survey.js
+++ models/survey.js
@@ -1,4 +1,15 @@
-let surveys = [];
+let surveys = [
+    // Example survey structure
+    // {
+    //   id: 1,
+    //   title: "Customer Feedback",
+    //   questions: ["How satisfied are you?", "Would you recommend us?"],
+    //   responses: [
+    //     { user: 'user1', answers: ['Very satisfied', 'Yes'] }
+    //   ],
+    //   creator: "user1" // Username of the survey creator
+    // }
+];
 
 module.exports = {
     surveys
